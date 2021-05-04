4f2bd901da11aa50e53f2092d84e59d11fd10da0
Yashyasvi Agarwal <yashyasviagarwal>
2021-05-04 14:35:12 +0530

    Cowin App Backend v1

---
+++ app.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+const axios = require("axios");
+const client = require("twilio")();
+console.log("Server Started");
+const timerID = setInterval(() => {
+  const district_id = 730;
+  let date = new Date();
+  if (date < new Date(2021, 4, 26)) {
+    date = new Date(2021, 4, 26);
+  }
+  date = date.toISOString().slice(0, 10).split("-").reverse().join("-");
+  let records = [];
+  axios
+    .get(
+      `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${district_id}&date=${date}`
+    )
+    .then((response) => {
+      response.data.centers.map((center) => {
+        center.sessions.map((session) => {
+          if (session.available_capacity >= 2) {
+            records.push("Center: " + center.name);
+            records.push("Date: " + session.date);
+            records.push("Available Seats: " + session.available_capacity);
+            records.push("Min_Age: " + session.min_age_limit);
+            records.push("");
+          }
+        });
+      });
+      if (records.length > 0) {
+        records = records.join("\n");
+        const messages = records.match(/(.|[\r\n]){1,1600}/g);
+        const numbers = ["+918335022832", "+918334825245"];
+        numbers.map((number) => {
+          messages.map((message) => {
+            client.messages
+              .create({
+                from: "whatsapp:+14155238886",
+                body: message,
+                to: `whatsapp:${number}`,
+              })
+              .then((message) => console.log("Message Sent: " + message.sid))
+              .catch((err) => {
+                console.log(err);
+                clearInterval(timerID);
+              });
+          });
+        });
+      } else {
+        console.log("No Available Slots");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      clearInterval(timerID);
+    });
+}, 60 * 1000);
